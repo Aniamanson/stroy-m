0dba962541699568b4124b924de3ed0d69735177
Vershkov Egor <evn88fx64@gmail.com>
2019-01-04 01:54:54 +0400

    slider beta

--- js/main.js
+++ js/main.js
@@ -5,38 +5,53 @@
     let portfolioModal = document.getElementById('portfolio-modal');
     let sliderView = document.getElementsByClassName('slides');
     let sliderThumbs = document.getElementsByClassName('slider-img');
-    let albumsPath = 'img/';
+    let activeFolder = 'nl';
     let activeImg = 0;
 
     function open(name, folder){
         hiddenPortfolio();
+        activeFolder = folder;
 
         let albumName = document.getElementById('albumName');
         albumName.textContent = getAlbumName(name);
 
         //загружаем список изображений слайдера
-        let path = albumsPath + folder + '/';
-        sliderView[0].style.backgroundImage = 'url('+ path + '0' + '.jpg)';
+        sliderView[0].style.backgroundImage = 'url(img/'+ activeFolder + '/0.jpg)';
         
         //превьюшки к сладеру
         for (let i = 0; i < 6; i++ ){
-            sliderThumbs[i].style.backgroundImage = 'url('+ path + i + '.jpg)';
+            sliderThumbs[i].style.backgroundImage = 'url(img/' + activeFolder + '/' + i +'.jpg)';
+            sliderThumbs[i].setAttribute('id', i);
+            sliderThumbs[i].addEventListener('click', handler);
+
         }
-        // console.log(name, folder);
     }
+
+    function nextImg() {
+        (activeImg >= 5) ? activeImg = 0 : activeImg++;
+        sliderView[0].style.backgroundImage = 'url(img/'+ activeFolder + '/'+ activeImg +'.jpg)';
+    }
+
+    function prevImg() {
+        (activeImg <= 0) ? activeImg = 5 : activeImg--;
+        sliderView[0].style.backgroundImage = 'url(img/'+ activeFolder + '/'+ activeImg +'.jpg)';
+    }
+
+    function handler(e){
+        activeImg = e.srcElement.id;
+        sliderView[0].style.backgroundImage = e.srcElement.style.backgroundImage;
+        // TODO: доделать смену класса для активного изображения
+        e.srcElement.classList.add('img-active');
+    }
+
 
     function closeModal (){
         portfolioModal.classList.remove('visible');
         portfolioModal.classList.add('hidden');
         portfolio.classList.remove('hidden');
+        activeImg = 0;
     };
 
-    function checkImg (path){
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', path, false);
-        xhr.send();
-        return (xhr.status != 200);
-    }
     function getAlbumName (name){
         return name.parentNode.parentNode.getElementsByTagName('p')[0].textContent;
 
@@ -47,19 +62,12 @@
         portfolioModal.classList.remove('hidden');
         portfolioModal.classList.add('visible');
     };
-    
-    // let xhr = new XMLHttpRequest();
-    // xhr.open('GET', albumsPatch + name + '/album_name.txt', false);
-    // xhr.send();
-    // if (xhr.status != 200) {
-    //     alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-    // } else {
-    //     return (xhr.responseText);
-    // }
 
     //export
     modal.open = open;
     modal.close = closeModal;
+    modal.nextImg = nextImg;
+    modal.prevImg = prevImg;
     window.Modal = modal;
 }());
 
@@ -68,13 +76,17 @@ document.onkeydown = function (e) {
         case 27: console.log('ESC'); 
             Modal.close();
             break;
-        case 39: console.log('right'); break;
-        case 37: console.log('left'); break;
+        case 39: console.log('right'); 
+            Modal.nextImg();
+            break;
+        case 37: console.log('left'); 
+            Modal.prevImg();
+            break;
     }
     // console.log(e.keyCode);
 }
 
 $(function () {
     $(".tel").mask("+7(000) 000-00-00");
-  });
+});
 
