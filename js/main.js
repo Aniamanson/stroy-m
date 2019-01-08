6da739536dd56604908558ca864ba63fd4e681a6
Ann Vershkova <aniamanson@gmail.com>
2019-01-08 20:52:34 +0400

    add animate

--- js/main.js
+++ js/main.js
@@ -132,3 +132,35 @@
     exportСarousel.prev = prev;
     window.Carousel = exportСarousel;
 }());
+
+
+
+$(document).ready(function(){
+	$("nav").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $("#button-up").on("click", function () {
+        $('body,html').animate({scrollTop: 0}, 1500);
+    });
+
+    $("#scroll").on("click", function () {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    
+    
+});
+
